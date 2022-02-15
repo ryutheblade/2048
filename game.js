49738ec783b4b875880ad71d4cf0e22c6dc0cb33
ryutheblade <65879071+ryutheblade@users.noreply.github.com>
2022-02-15 15:40:29 -0800

    Initial commit: created grid that generates initial numbers

---
+++ game.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.getElementById('score')
+    const resultDisplay = document.getElementById('result')
+    const width = 4
+    let squares = []
+
+    //play board
+    function board() {
+        for(let i=0; i < width*width; i++) {
+            square = document.createElement('div')
+            square.innerHTML = 0
+            gridDisplay.appendChild(square)
+            squares.push(square)
+        }
+        generate()
+        generate()
+    }
+    board()
+
+    //create random number
+    function generate() {
+        randomNumber = Math.floor(Math.random() * squares.length)
+        if (squares[randomNumber].innerHTML == 0) {
+            squares[randomNumber].innerHTML = 2
+        } else generate()
+    }
+
+})
